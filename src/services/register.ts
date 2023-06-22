c29fff2274d14aa30d5bba41da6a7840048d9598
caiocapua <caiocapua@hotmail.com>
2023-06-21 21:43:22 -0300

    create service, controller and repository for users

---
+++ src/services/register.ts
@@ -0,0 +1,43 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
+
+interface RegisterServiceRequest {
+  name: string
+  email: string
+  password: string
+}
+
+interface RegisterServiceResponse {
+  user: User
+}
+
+export class RegisterService {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterServiceRequest): Promise<RegisterServiceResponse> {
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    const passwordHash = await hash(password, 6)
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password_hash: passwordHash,
+    })
+
+    return {
+      user,
+    }
+  }
+}
